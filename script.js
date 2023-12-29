07cd19ca13007a6c7eb654631694cc8c08635463
Facundo <duhaufacundoarg@gmail.com>
2023-12-28 21:21:14 -0300

    Added user prompt for choice

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function getComputerChoice(){
+function getComputerChoice() // function to generate the computers choice, inside the given items of the array
+{
     let number = Math.floor(Math.random()*3);
     console.log(number);
-    let choiceList = ["Rock", "Paper", "Scissors"];
+    let choiceList = ["ROCK", "PAPER", "SCISSORS"];
     let choice = choiceList[number];
-    console.log(choice);
     return choice;
 }
-getComputerChoice();
+
+function playerChoice() // function to prompt the user for a choice
+{
+        let playerChoice = prompt("What will you play? ").toUpperCase();
+        return playerChoice;
+}
+
+var choice = getComputerChoice();   // Get the values returned by the functions
+var playerChoice = playerChoice();  //
+
+console.log(choice); // show the results
+console.log(playerChoice);
